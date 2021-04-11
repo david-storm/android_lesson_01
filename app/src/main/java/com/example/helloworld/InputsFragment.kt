3bc3a8ccd43e8836eb98c5656338cdf16b3ad25f
david-storm <venia7vedmak@gmail.com>
2021-04-11 16:30:27 +0300

    layout first screen and add navigation

---
+++ app/src/main/java/com/example/helloworld/InputsFragment.kt
@@ -0,0 +1,22 @@
+package com.example.helloworld
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.helloworld.databinding.FragmentInputsBinding
+
+class InputsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding =
+            DataBindingUtil.inflate<FragmentInputsBinding>(inflater, R.layout.fragment_inputs, container, false)
+
+        return binding.root
+    }
+}
---
+++ app/src/main/java/com/example/helloworld/RadiosFragment.kt
@@ -0,0 +1,24 @@
+package com.example.helloworld
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.helloworld.databinding.FragmentRadiosBinding
+
+class RadiosFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding =
+            DataBindingUtil.inflate<FragmentRadiosBinding>(inflater, R.layout.fragment_radios, container, false)
+
+        return binding.root
+    }
+
+}
