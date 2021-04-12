b175abf590a4856afb51fd61b0179441d61d81dd
david-storm <venia7vedmak@gmail.com>
2021-04-12 23:06:10 +0300

    add logic with inputs

--- app/src/main/java/com/example/helloworld/InputsFragment.kt
+++ app/src/main/java/com/example/helloworld/InputsFragment.kt
@@ -1,12 +1,15 @@
 package com.example.helloworld
 
+import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import com.example.helloworld.databinding.FragmentInputsBinding
+
 
 class InputsFragment : Fragment() {
 
@@ -15,8 +18,24 @@ class InputsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val binding =
-            DataBindingUtil.inflate<FragmentInputsBinding>(inflater, R.layout.fragment_inputs, container, false)
+            DataBindingUtil.inflate<FragmentInputsBinding>(
+                inflater,
+                R.layout.fragment_inputs,
+                container,
+                false
+            )
+        binding.button.text = getString(R.string.hide_keyboard, System.getProperty("line.separator"))
+        binding.button.setOnClickListener {
+            hideKeyboard(it)
+        }
 
         return binding.root
+    }
+
+    fun hideKeyboard(view: View){
+        val imm =
+            requireContext().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+
     }
 }
