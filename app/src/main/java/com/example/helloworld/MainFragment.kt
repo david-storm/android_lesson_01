ee0b3508a33809eb3c36f5779def5c87cba58396
david-storm <53839475+david-storm@users.noreply.github.com>
2021-04-13 17:06:16 +0300

    layout first screen and add navigation (#1)
    * layout first screen and add navigation
    
    * remove wrong dependence

---
+++ app/src/main/java/com/example/helloworld/MainFragment.kt
@@ -0,0 +1,37 @@
+package com.example.helloworld
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import com.example.helloworld.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = DataBindingUtil.inflate<FragmentMainBinding>(
+            inflater,
+            R.layout.fragment_main, container, false
+        )
+
+        binding.buttonToButtons.setOnClickListener{ view: View ->
+            view.findNavController().navigate(R.id.action_mainFragment_to_buttonsFragment)
+        }
+        binding.buttonToinputs.setOnClickListener{ view: View ->
+            view.findNavController().navigate(R.id.action_mainFragment_to_inputsFragment)
+        }
+        binding.buttonToRadios.setOnClickListener{ view: View ->
+            view.findNavController().navigate(R.id.action_mainFragment_to_radiosFragment)
+        }
+
+        return binding.root
+    }
+
+}
